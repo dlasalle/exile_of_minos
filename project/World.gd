ab45f03dfe6c0ecf95af3fa781b42dd95e9239be
Dominique LaSalle <dominique@solidlake.com>
2018-10-03 23:47:57 -0700

    Add minimap

--- project/World.gd
+++ project/World.gd
@@ -1,25 +1,30 @@
 extends Spatial
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var m_time = 0
+
+func timestamp():
+	return OS.get_unix_time()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var grid = $GridMap
+	var grid = $Viewport/GridMap
 	
 	var m2d = Maze2D.new()
-	m2d.set_seed(2)
+	m2d.set_seed(timestamp())
 	var width = 10
 	var length = 10
-	var border_height = 3
+	var border_height = 4
+	
+	$Viewport/TopCamera.size = width*4+1
+	$Viewport/TopCamera.transform.origin.x = width*2-1
+	$Viewport/TopCamera.transform.origin.z = length*2-1
 	
 	m2d.build(grid, width, length, 0)
 
 	# add floor	
 	for y in range(0, length*2-1):
 		for x in range(0, width*2-1):
-			grid.set_cell_item(x,-1,y, 0)
+			grid.set_cell_item(x,-1,y, 1)
 
 	# add walls
 	for z in range(0, border_height):
@@ -32,10 +37,18 @@ func _ready():
 			grid.set_cell_item(x, z, length*2-1, 0)
 
 	# add start
-	grid.set_cell_item(-1, -1, 0, 0)
-	grid.set_cell_item(-1, 0, 0, -1)
-	grid.set_cell_item(-1, 4, 0, 0)
+	grid.set_cell_item(-1, -1, 0, 1)
+	for z in range(0, border_height):
+		grid.set_cell_item(-1, z, 0, -1)
+
 	
-	grid.set_cell_item(width*2-2, -1, length*2-1, 0)
-	grid.set_cell_item(width*2-2, 0, length*2-1, -1)
-	grid.set_cell_item(width*2-2, 4, length*2-1, 0)
+	grid.set_cell_item(width*2-2, -1, length*2-1, 1)
+	for z in range(0, border_height):
+		grid.set_cell_item(width*2-2, z, length*2-1, -1)
+
+func _physics_process(delta):
+	m_time += delta
+	var hours = int(m_time / (60*60))
+	var minutes = int((m_time/60) - (hours*60))
+	var seconds = m_time - (((hours*60)+minutes)*60)
+	$Label.text = "%02d:%02d:%02.1f" % [hours, minutes, seconds]
